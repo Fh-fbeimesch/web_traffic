16b12f2617b9dd657b93e99dd073aba427c5579e
Fh-fbeimesch <fbeimesch@fischerhomes.com>
2017-06-13 15:53:02 +0000

    add Yii2 files

---
+++ views/traffic/_form.php
@@ -0,0 +1,46 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use dosamigos\datepicker\DatePicker;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Traffic */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="traffic-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+    <!-- placeholder is not working currently -->
+    <!--<?= $form->field($model, 'week')->textInput()->input(['placeholder'=> "YYYY-MM-DD (ex. 2016-01-24)"]) ?>-->
+
+    <!-- dosamigos datepicker -->
+    <?= $form->field($model, 'week')->widget(
+    DatePicker::className(), [
+        // inline too, not bad
+         'inline' => false, 
+         // modify template for custom rendering
+       // 'template' => '<div class="well well-sm" style="background-color: #fff; width:250px">{input}</div>',
+        'clientOptions' => [
+            'autoclose' => true,
+            'format' => 'yyyy-mm-dd'
+        ]
+]);?>
+
+
+
+    YYYY-MM-DD (ex. 2016-01-24)
+    <br>
+    <br>
+
+
+    <?= $form->field($model, 'traffic')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ views/traffic/index.php
@@ -0,0 +1,34 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\TrafficSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Traffic';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="traffic-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Traffic', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'week',
+            'traffic',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
